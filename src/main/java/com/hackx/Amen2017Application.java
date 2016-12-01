29d6818bbadd9b6114629efc8140342ee9b2efc7
caolei.cl <caolei.cl@alibaba-inc.com>
2016-12-01 16:06:59 +0800

    first commit

---
+++ src/main/java/com/hackx/Amen2017Application.java
@@ -0,0 +1,12 @@
+package com.hackx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Amen2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Amen2017Application.class, args);
+	}
+}
